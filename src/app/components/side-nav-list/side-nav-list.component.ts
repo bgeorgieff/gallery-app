8b5195cca253cc93e424d7a74367ba13c21ceb49
Blagovest Georgiev <b.g.georgiev87@gmail.com>
2022-07-21 16:12:54 +0300

    added logout to hamburger menu

--- src/app/components/side-nav-list/side-nav-list.component.ts
+++ src/app/components/side-nav-list/side-nav-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, Output, EventEmitter } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: "app-side-nav-list",
@@ -8,7 +10,18 @@ import { Component, Output, EventEmitter } from "@angular/core";
 export class SideNavListComponent {
   @Output() closeSideNav = new EventEmitter();
 
+  constructor(private userService: UserService, private router: Router) {}
+
   sideNavClose() {
     this.closeSideNav.emit();
+  }
+
+  isLoggedIn() {
+    return this.userService.isLoggedIn();
+  }
+
+  logOut() {
+    this.userService.logOut();
+    this.router.navigate(["/"]);
   }
 }
