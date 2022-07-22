a169b691a2dd3abe1c5f6b9b4360011aae1f4741
Blagovest Georgiev <b.g.georgiev87@gmail.com>
2022-07-22 09:41:38 +0300

    about page and delete action on pictures

--- src/app/modules/about/pages/my-story/my-story.component.ts
+++ src/app/modules/about/pages/my-story/my-story.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PageTitles } from "src/app/enums/page-titles.enum";
+import { TitleService } from "src/app/services/title.service";
 
 @Component({
-  templateUrl: './my-story.component.html',
-  styleUrls: ['./my-story.component.scss']
+  templateUrl: "./my-story.component.html",
+  styleUrls: ["./my-story.component.scss"],
 })
-export class MyStoryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MyStoryComponent {
+  constructor(private titleService: TitleService) {
+    this.titleService.setTitle(PageTitles.aboutMe);
   }
-
 }
--- src/app/modules/gallery/pages/gallery/gallery.component.ts
+++ src/app/modules/gallery/pages/gallery/gallery.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnDestroy, OnInit, Renderer2 } from "@angular/core";
 import { Router } from "@angular/router";
-import { Subscription } from "rxjs";
+import { findIndex, Subscription } from "rxjs";
 import { Assets } from "src/app/enums/assets.enum";
 import { Classes } from "src/app/enums/classes.enum";
+import { MessageType } from "src/app/enums/message-types.enum";
 import { PageTitles } from "src/app/enums/page-titles.enum";
 import { ICard } from "src/app/interfaces/card.interface";
 import { GalleryService } from "src/app/services/gallery.service";
 import { ImageViewService } from "src/app/services/image-view.service";
 import { LoaderService } from "src/app/services/loader.service";
 import { TitleService } from "src/app/services/title.service";
+import { ToastrService } from "src/app/services/toastr.service";
 import { UserService } from "src/app/services/user.service";
 
 @Component({
@@ -28,7 +30,8 @@ export class GalleryComponent implements OnInit, OnDestroy {
     private titleService: TitleService,
     private renderer: Renderer2,
     private loaderService: LoaderService,
-    private imageViewService: ImageViewService
+    private imageViewService: ImageViewService,
+    private toastrService: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -75,6 +78,24 @@ export class GalleryComponent implements OnInit, OnDestroy {
 
   navigateToEdit(painting: ICard) {
     this.router.navigate([`/gallery/edit/${painting._id}`]);
+  }
+
+  deletePainting(painting: ICard) {
+    const id = painting._id || "";
+    this.subscriptions.push(
+      this.galleryService.deletePainting(id).subscribe({
+        next: (response) => {
+          this.paintings = this.paintings.filter(
+            (painting) => painting._id !== id
+          );
+          const { message } = response;
+          this.toastrService.showMessage(message, MessageType.success);
+        },
+        error: (error) => {
+          throw new Error(error);
+        },
+      })
+    );
   }
 
   ngOnDestroy(): void {
